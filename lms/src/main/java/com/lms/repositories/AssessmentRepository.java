1372b4629dd5f1b0c21fb5eed0f3f6e8ab5c2087
Carlos Oliveros <caoliverosj@unimagdalena.edu.co>
2026-03-16 12:55:53 -0500

    querys assessmentRepository

--- lms/src/main/java/com/lms/repositories/AssessmentRepository.java
+++ lms/src/main/java/com/lms/repositories/AssessmentRepository.java
@@ -2,6 +2,32 @@ package com.lms.repositories;
 
 import com.lms.entities.Assessment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.Instant;
+import java.util.List;
 
 public interface AssessmentRepository extends JpaRepository<Assessment, Long> {
+
+    @Query("""
+    select a
+    from Assessment a
+    where a.student.id = :studentId 
+    AND a.takenAt between :start AND :end
+    """)
+    List<Assessment> findAssessmentByStudendIdAndBetweenTakenAt(@Param("studentId") Long studentId,
+                                                                @Param("start") Instant start,
+                                                                @Param("end") Instant end);
+
+    @Query("""
+    select a
+    from Assessment a
+    where a.student.id = :studentId
+    AND a.score BETWEEN :min AND :max
+""")
+    List<Assessment> findAssessmentByStudentIdAndBetweenScore(@Param("studentId") Long studentId,
+                                                              @Param("min") int min,
+                                                              @Param("max") int max);
+
 }
