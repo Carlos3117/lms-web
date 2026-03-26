2ab4674f99c4692e56b3de4bf4864c0c0e677d77
Carlos Oliveros <caoliverosj@unimagdalena.edu.co>
2026-03-25 22:01:49 -0500

    instructorprofilleservice modificated

--- lms/src/main/java/com/lms/services/InstructorProfileService.java
+++ lms/src/main/java/com/lms/services/InstructorProfileService.java
@@ -1,4 +1,38 @@
 package com.lms.services;
 
+import com.lms.entities.InstructorProfile;
+
+import java.util.List;
+
 public interface InstructorProfileService {
+
+    InstructorProfile create(InstructorProfile profile);
+
+    InstructorProfile update(Long id, InstructorProfile profile);
+
+    InstructorProfile findById(Long id);
+
+    List<InstructorProfile> findAll();
+
+    void delete(Long id);
+
+    InstructorProfile findByInstructor(Long instructorId);
+
+    boolean existsByInstructor(Long instructorId);
+
+    class ProfileNotFoundException extends RuntimeException {
+        public ProfileNotFoundException(String message) { super(message); }
+    }
+
+    class ProfileInvalidException extends RuntimeException {
+        public ProfileInvalidException(String message) { super(message); }
+    }
+
+    class ProfileAlreadyExistsException extends RuntimeException {
+        public ProfileAlreadyExistsException(String message) { super(message); }
+    }
+
+    class ProfileConflictException extends RuntimeException {
+        public ProfileConflictException(String message) { super(message); }
+    }
 }
